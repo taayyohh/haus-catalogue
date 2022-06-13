24ae01453696b7c4403ae101f56b3c84d13dc9df
Théo Mode <milesxbridges@gmail.com>
2022-06-12 22:14:48 -0400

    ux logic for bundlr uploader

--- pages/upload/index.tsx
+++ pages/upload/index.tsx
@@ -12,83 +12,159 @@ const Upload = () => {
         return new Bundlr("https://node1.bundlr.network", "matic", provider)
     }, [provider])
 
+    const [instance, setInstance] = React.useState(null)
+    const [walletBalance, setWalletBalance] = React.useState(0)
+    const getBalance = React.useMemo(async() => {
+        if(!instance) return
+
+        const balance = await instance.getLoadedBalance()
+        setWalletBalance(parseFloat(ethers.utils.formatEther(balance.toString())))
+    }, [instance])
+
+
+
+
     const handleSubmit = (values: { file: {}; }) => {
         createBundlrTx(values.file, values.file.type)
     }
 
     const [buffer, setBuffer] = React.useState(null)
+    const [txId, setTxId] = React.useState(null)
+    const [priceOfUpload, setPriceOfUpload] = React.useState(0)
+
+    const getPrice = React.useCallback(async (files) => {
+        try {
+            const price = await instance.getPrice(files[0]?.size)
+            setPriceOfUpload(parseFloat(ethers.utils.formatEther(price.toString())))
+        } catch (err) {
+            console.log('err', err)
+        }
+
+    }, [instance])
+
+    const getBuffer = React.useCallback(async (file) => {
+        const tags = [{name: "Content-Type", value: file.type}]
+        const blob = new Blob([file], {type: file.type})
+
+        let reader = new FileReader()
+        reader.onload = async function(evt) {
+            console.log(evt.target.result);
+            setBuffer(evt.target.result)
+        }
+        reader.readAsArrayBuffer(blob)
+
+    }, [instance])
+
     const createBundlrTx = async (data, value: string) => {
         try {
-            const bundlrInstance = await bundlr
-            if(!bundlrInstance) return
-
-            await bundlrInstance.ready()
             await provider._ready()
-            console.log('add', bundlrInstance.address)
-
             const tags = [{name: "Content-Type", value}]
-            const blob = new Blob([data], {type: data.type})
-            // const localURL = URL.createObjectURL(data)
 
-            let reader = new FileReader()
-            reader.onload = async function(evt) {
-                console.log(evt.target.result);
-                setBuffer(evt.target.result)
-            }
-            reader.readAsArrayBuffer(blob)
-
-            const price = await bundlrInstance.getPrice(data.size)
-            const balance = await bundlrInstance.getLoadedBalance()
-            console.log('ba', balance)
-            if (price.isGreaterThan(balance)) {
-                const fund = await bundlrInstance.fund((parseFloat(ethers.utils.formatEther(price.toString())) * 2) * 10 ** 18, 1.1)
-                console.log('fund', fund)
-            }
-           const ex = await execute(await bundlrInstance, tags)
+            const ex = await execute(await instance, tags)
+            setTxId(ex)
 
         } catch(err) {
             console.log('err', err)
         }
     }
 
-    const execute = React.useCallback(async (bundlrInstance: any, tags: any) => {
+    const execute = React.useCallback(async (instance: any, tags: any) => {
         if(!buffer) return
 
-        console.log('b', bundlrInstance, tags, buffer)
-
-        const tx = bundlrInstance.createTransaction(buffer, {tags: tags});
+        const tx = instance.createTransaction(buffer, {tags: tags});
         await tx.sign();
         await tx.upload()
-        const id = (await tx.upload()).data.id
-        console.log('tx', tx, id)
-
-        return id
+        return (await tx.upload()).data.id
 
     }, [buffer])
 
+    const handleFund = React.useCallback(async(price) => {
+        const amount = ethers.utils.parseEther(price.toString()).toString()
+        const fund = await instance.fund(amount, 1.1)
+        await getBalance
 
-    return (
+    }, [instance])
+
+    /* styles */
+    const button = "inline-flex self-start p-4 bg-slate-400 rounded-xl hover:bg-slate-500"
+
+    // uploads
+    //https://arweave.net/jUxBkBgjBSk7KCwuYjVrr-ByHWf8EMO7YK_kFtPvzAU
+    //https://arweave.net/ouL0JHLPI83jh54LZt9YA8zKhbP1EdqIfkBM3iXxwng
+    //https://arweave.net/y2zVLLIybgci6pwj7PPKA1bGTWESB_oHQAXRMxsvvB8
+    //http://localhost:3000/upload
+
+
+        return (
         <div>
-            hi
-            <Formik
-                initialValues={{ file: {} }}
-                onSubmit={(values) => handleSubmit(values)}
-            >
-                {props => (
-                    <form onSubmit={props.handleSubmit}>
-                        <>
-                            <input
-                                name="file"
-                                type="file"
-                                onChange={(event) => {
-                                    props.setFieldValue("file", event.currentTarget.files[0]);
-                                }}
-                            />
-                            <button type="submit">submit</button>
-                        </>
-                    </form>
-                )}
-            </Formik>
+            {!instance && (
+                <button
+                    type="button"
+                    className={button}
+                    onClick={async () => {
+                        const instance = await bundlr
+                        if(!!instance) {
+                            await instance.ready()
+                            await setInstance(instance)
+                        }
+                    }}
+                >
+                    Connect to Bundlr
+                </button>
+            )}
+
+
+            {!!instance && (
+                <div>
+                    <div>
+                        Bundlr Wallet Funded with: {walletBalance} MATIC
+                    </div>
+                    <div>
+                        Price of Upload: {priceOfUpload} MATIC
+                    </div>
+                    {priceOfUpload - walletBalance > 0 ? <div>
+                        <div>Funding Needed: {priceOfUpload - walletBalance}</div>
+                        <button type="button" onClick={() => handleFund(priceOfUpload)}>
+                            Fund
+                        </button>
+
+                    </div>: null}
+                </div>
+            )}
+
+            {!!instance && (
+                <div>
+                    <div>Upload File</div>
+                    <Formik
+                        initialValues={{ file: {} }}
+                        onSubmit={(values) => handleSubmit(values)}
+                    >
+                        {props => (
+                            <form onSubmit={props.handleSubmit}>
+                                <>
+                                    <input
+                                        name="file"
+                                        type="file"
+                                        onChange={(event) => {
+                                            props.setFieldValue("file", event.currentTarget.files[0]);
+                                            getPrice(event.currentTarget.files)
+                                            getBuffer(event.currentTarget.files[0])
+                                        }}
+                                    />
+                                    {priceOfUpload && walletBalance && (priceOfUpload - walletBalance <= 0) ? (
+                                        <button type="submit">submit</button>
+                                    ) : null}
+                                    {!!txId ? <div>
+                                        <a href={`https://arweave.net/${txId}`}>{`https://arweave.net/${txId}`}</a>
+                                    </div> : null}
+                                </>
+                            </form>
+                        )}
+                    </Formik>
+                </div>
+            )}
+
+
         </div>
     )
 
