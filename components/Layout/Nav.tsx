4a7f8fd447d42c752bd7191b2eb9d03125c98e51
Théo Mode <milesxbridges@gmail.com>
2022-06-16 12:32:05 -0400

    style improvments

--- components/Layout/Nav.tsx
+++ components/Layout/Nav.tsx
@@ -6,21 +6,21 @@ const Nav = () => {
   const { formattedDate } = useRelativeTime()
 
   return (
-    <div className="fixed z-10 flex w-full justify-between bg-rose-300 p-4">
+    <div className="fixed z-10 flex w-full items-center justify-center bg-rose-300 p-4">
       <input
-        className="focus:shadow-outline w-36 rounded-xl bg-rose-200 px-4 text-rose-800 focus:outline-none"
+        className="absolute left-4 focus:shadow-outline w-36 rounded h-8 bg-rose-200 px-4 text-rose-800 focus:outline-none"
         placeholder="Search"
       />
 
       <div>
-        <div className="w-28">
+        <div className="w-24">
           <img src="/lucidhaus.png" />
-          <div>{formattedDate(new Date())}</div>
+          {/*<div>{formattedDate(new Date())}</div>*/}
         </div>
       </div>
 
-        <div id="connect">
-            <ConnectButton showBalance={false} label={"Connect"} chainStatus={"none"} accountStatus={"address"} />
+        <div id="connect" className="absolute right-4">
+            <ConnectButton showBalance={true} label={"Connect"} chainStatus={"none"} accountStatus={"address"} />
         </div>
     </div>
   )
