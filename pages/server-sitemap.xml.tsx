2aa2443f8d0ccd44f74c6991479cf9203b8a4318
taayyohh <milesxbridges@gmail.com>
2023-03-15 09:41:39 -0400

    add sitemap

---
+++ pages/server-sitemap.xml.tsx
@@ -0,0 +1,26 @@
+import { ReleaseProps } from 'data/query/typings'
+import { GetServerSideProps } from 'next'
+import { ISitemapField, getServerSideSitemapLegacy } from 'next-sitemap'
+import { slugify } from 'utils'
+
+import { getDiscography } from 'modules/song'
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { discography } = await getDiscography()
+  const d = discography as ReleaseProps[]
+
+  const fields = d?.reduce((acc: ISitemapField[], cv: ReleaseProps) => {
+    acc.push({
+      loc: `https://lucid.haus/${slugify(cv.metadata.artist)}/${slugify(cv.name)}`,
+      lastmod: new Date().toISOString(),
+      changefreq: 'daily',
+      priority: 0.07,
+    })
+
+    return acc
+  }, [])
+
+  return getServerSideSitemapLegacy(ctx, fields)
+}
+
+export default function Sitemap() {}
